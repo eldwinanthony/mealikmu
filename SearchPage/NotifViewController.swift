6b63c9c85447caf8455f6373f706f47164d6bc2a
eldwinanthony <eldwinanthony@gmail.com>
2022-04-07 23:01:26 +0700

    Udah bikin page notif sama sambungin

--- SearchPage/NotifViewController.swift
+++ SearchPage/NotifViewController.swift
@@ -5,16 +5,39 @@
 //  Created by Eldwin Anthony on 07/04/22.
 //
 
-import Foundation
 import UIKit
 
 class NotifViewController: UIViewController {
     
-    
+    @IBOutlet weak var reminderBtn: UIButton!
+    @IBOutlet weak var achievementBtn: UIButton!
+    @IBOutlet weak var announcementBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //btn shadow and corner
+        reminderBtn.layer.cornerRadius = 20
+        reminderBtn.layer.shadowColor = UIColor.gray.cgColor
+        reminderBtn.layer.shadowOpacity = 0.6
+        reminderBtn.layer.shadowOffset = .zero
+        reminderBtn.layer.shadowRadius = 2
+        
+        achievementBtn.layer.cornerRadius = 20
+        achievementBtn.layer.shadowColor = UIColor.gray.cgColor
+        achievementBtn.layer.shadowOpacity = 0.6
+        achievementBtn.layer.shadowOffset = .zero
+        achievementBtn.layer.shadowRadius = 2
+        
+        announcementBtn.layer.cornerRadius = 20
+        announcementBtn.layer.shadowColor = UIColor.gray.cgColor
+        announcementBtn.layer.shadowOpacity = 0.6
+        announcementBtn.layer.shadowOffset = .zero
+        announcementBtn.layer.shadowRadius = 2
+
         // Do any additional setup after loading the view.
     }
-
     
+
+
 }
--- SearchPage/SearchViewController.swift
+++ SearchPage/SearchViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SearchViewController: UIViewController {
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
