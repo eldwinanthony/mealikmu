d90664af6ae217a98179d81f07101f7338054650
fauziardha1 <fauziardasaputra@gmail.com>
2022-04-11 15:31:50 +0700

    init data struct for meal data

---
+++ SearchPage/MealDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  MealDataModel.swift
+//  SearchPage
+//
+//  Created by FauziArda on 11/04/22.
+//
+
+import Foundation
+struct Meal{
+    var imageName: String?
+    var mealName: String?
+    var cookingTime: Int
+    var description: String?
+    var ingredients: [String]
+    var equipments: [String]
+    var recipes: [String]
+    
+}
