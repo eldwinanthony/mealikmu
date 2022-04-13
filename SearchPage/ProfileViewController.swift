dc4c456ba0241232075b5658449fc64d0ad8899f
eldwinanthony <eldwinanthony@gmail.com>
2022-04-13 20:36:23 +0700

    terupdate food detail uda connect tpi belom muncul

--- SearchPage/ProfileViewController.swift
+++ SearchPage/ProfileViewController.swift
@@ -11,22 +11,26 @@ class ProfileViewController: UIViewController {
     
     //reference
     var mvController = MealViewController()
-    
+    var automatedController = AutomatedPageViewController()
     //Outlets
     @IBOutlet weak var nameprofileLabel: UILabel!
     @IBOutlet weak var Banne: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var preptimeTextField: UITextField!
+    
     
     override func viewDidLoad() {
         
         //layout profileuiview
       //  var backgroundColor = UIColor.gray.cgColor
         
+        nameTextField.text = mvController.userNameVar
         
         Banne.layer.shadowColor = UIColor.gray.cgColor
         Banne.layer.shadowOpacity = 0.2
         Banne.layer.shadowOffset = .zero
         Banne.layer.shadowRadius = 10
-     
+        
         
         nameprofileLabel.text = "Hi, \(mvController.userNameVar)!"
         
