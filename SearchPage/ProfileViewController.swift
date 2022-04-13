f96eb183087b4224c653c5e0b4f1ab8f9ccc610a
eldwinanthony <eldwinanthony@gmail.com>
2022-04-13 13:47:34 +0700

    edit profile

--- SearchPage/ProfileViewController.swift
+++ SearchPage/ProfileViewController.swift
@@ -14,47 +14,17 @@ class ProfileViewController: UIViewController {
     
     //Outlets
     @IBOutlet weak var nameprofileLabel: UILabel!
-    @IBOutlet weak var profileUIView: UIView!
-    @IBOutlet weak var preptimeTextField: UITextField!
-    @IBOutlet weak var mealtime1TextField: UITextField!
-    @IBOutlet weak var mealtime2TextField: UITextField!
-    @IBOutlet weak var mealtime3TextField: UITextField!
+    @IBOutlet weak var Banne: UIImageView!
     
     override func viewDidLoad() {
         
         //layout profileuiview
-  
         
-        profileUIView.layer.cornerRadius = 17
-        profileUIView.layer.shadowColor = UIColor.gray.cgColor
-        profileUIView.layer.shadowOpacity = 0.35
-        profileUIView.layer.shadowOffset = .zero
-        profileUIView.layer.shadowRadius = 4
+        Banne.layer.shadowColor = UIColor.gray.cgColor
+        Banne.layer.shadowOpacity = 0.2
+        Banne.layer.shadowOffset = .zero
+        Banne.layer.shadowRadius = 10
         
-        preptimeTextField.layer.cornerRadius = 17
-        preptimeTextField.layer.shadowColor = UIColor.black.cgColor
-        preptimeTextField.layer.shadowOpacity = 0.35
-        preptimeTextField.layer.shadowOffset = .zero
-        preptimeTextField.layer.shadowRadius = 4
-        
-        mealtime1TextField.layer.cornerRadius = 17
-        mealtime1TextField.layer.shadowColor = UIColor.black.cgColor
-        mealtime1TextField.layer.shadowOpacity = 0.35
-        mealtime1TextField.layer.shadowOffset = .zero
-        mealtime1TextField.layer.shadowRadius = 4
-        
-        mealtime2TextField.layer.cornerRadius = 17
-        mealtime2TextField.layer.shadowColor = UIColor.black.cgColor
-        mealtime2TextField.layer.shadowOpacity = 0.35
-        mealtime2TextField.layer.shadowOffset = .zero
-        mealtime2TextField.layer.shadowRadius = 4
-        
-        mealtime3TextField.layer.cornerRadius = 17
-        mealtime3TextField.layer.shadowColor = UIColor.black.cgColor
-        mealtime3TextField.layer.shadowOpacity = 0.35
-        mealtime3TextField.layer.shadowOffset = .zero
-        mealtime3TextField.layer.shadowRadius = 4
-
         
         nameprofileLabel.text = "Hi, \(mvController.userNameVar)!"
         
