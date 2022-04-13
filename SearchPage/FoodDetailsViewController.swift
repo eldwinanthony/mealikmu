b95c9791069a7d037bae9f625f262f023b32abc0
Delfina Paulin <delfinapaulin09@gmail.com>
2022-04-13 17:36:58 +0700

    bikin halaman food details

---
+++ SearchPage/FoodDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FoodDetailsViewController.swift
+//  SearchPage
+//
+//  Created by Delfina Paulin on 13/04/22.
+//
+
+import UIKit
+
+class FoodDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
