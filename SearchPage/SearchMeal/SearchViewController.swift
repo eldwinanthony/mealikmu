9efd7de6f186d7264d66eb09fae3dc18d30151cf
eldwinanthony <eldwinanthony@gmail.com>
2022-04-13 12:31:08 +0700

    fitur search sama automated beres. belum nyambung ke food detail

--- SearchPage/SearchMeal/SearchViewController.swift
+++ SearchPage/SearchMeal/SearchViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet var searchtableView: UITableView!
-    
+    @IBOutlet weak var searchBar: UISearchBar!
     var mealData = foodDetails
+    var filteredData: [Meal]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,24 +22,43 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         searchtableView.delegate = self
         searchtableView.dataSource = self
         searchtableView.rowHeight = 150
-
+        searchBar.delegate = self
         
+        filteredData = mealData
+
         // Do any additional setup after loading the view.
     }
     
     //tableView funcs
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mealData.count
+        return filteredData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
-        cell.foodNameLabel?.text = mealData[indexPath.row].mealName
-        cell.foodImage?.image = mealData[indexPath.row].mealImage
-        cell.foodDesc.text = mealData[indexPath.row].description
-        cell.cookingTime.text = "\(mealData[indexPath.row].cookingTime) Minutes"
+        cell.foodNameLabel?.text = filteredData[indexPath.row].mealName
+        cell.foodImage?.image = filteredData[indexPath.row].mealImage
+        cell.foodDesc.text = filteredData[indexPath.row].description
+        cell.cookingTime.text = "\(filteredData[indexPath.row].cookingTime) Minutes"
         
         return cell
+    }
+    
+    //search bar configuration
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = []
+        
+        if searchText == "" {
+            filteredData = mealData
+        }
+        else{
+        for meal in mealData {
+            if meal.mealName!.lowercased().contains(searchText.lowercased()){
+                filteredData.append(meal)
+                }
+            }
+        }
+        self.searchtableView.reloadData()
     }
 
 
